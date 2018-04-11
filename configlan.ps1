f2b9d19272b6593ffebcdfc76e18b495449471c3
lovellt <lovellt@users.noreply.github.com>
2018-04-11 14:23:08 -0400

    Update configlan.ps1

--- configlan.ps1
+++ configlan.ps1
@@ -1,5 +1,9 @@
+#download LAN profile
 wget https://raw.githubusercontent.com/lovellt/ISE_Config/master/HH_Wired.xml -outfile HH_Wired.xml
+#download WLAN profile
 wget https://raw.githubusercontent.com/lovellt/ISE_Config/master/HH_Wireless.xml -outfile HH_Wireless.xml
+
+#Check for LAN profile, start wired autoconfig, set wired autoconfig to auto, apply profile, delete profile file
 if ((Get-FileHash .\HH_Wired.xml -algorithm sha1).Hash -eq "F68D1D4F0C8EB1FA481EEB780C284D6576A084BD")
   {
     cmd /c "sc config dot3svc start=auto"
@@ -11,6 +15,7 @@ if ((Get-FileHash .\HH_Wired.xml -algorithm sha1).Hash -eq "F68D1D4F0C8EB1FA481E
   else 
   {write-host "HH_Wired.xml not found or corrupt, connect to the internet"}
 
+#Check for WLAN profile, apply profile, delete profile file
 if ((Get-FileHash .\HH_Wireless.xml -algorithm sha1).Hash -eq "54F4682037F100DDB16F5B5C8A4E8A10F3FC56D5")
   {  
     cmd /c netsh wlan add profile HH_Wireless.xml
@@ -31,7 +36,7 @@ if ((Get-FileHash .\HH_Wireless.xml -algorithm sha1).Hash -eq "54F4682037F100DDB
     #delete HHA1r
     netsh wlan delete profile name=HHA1r-5g
     
-    #Put HHAir-5G as the top priority (win10)
+    #Put HHAr1-Test as the top priority (win10)
     netsh wlan set profileorder name="HHAr1-Test" interface="Wi-Fi" priority=1
     
     
